c4e4bac983dbd8d1e67c4ce2c12ad278f4a39a9d
Akans Udoka <Akansudoka7@gmail.com>
2023-02-16 23:47:40 +0100

    Add project files.

---
+++ Data/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_Book.Data.Model;
+
+namespace My_Book.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) 
+        { 
+        
+        
+        }
+        public DbSet<Book> books { get; set; }
+    }
+}
